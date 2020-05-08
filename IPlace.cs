b4e6e850a2ff83f56849cf6226d45c84c04aede0
Ferry To <ferry@verdantsparks.io>
2020-05-08 23:12:39 +0800

    init

---
+++ IPlace.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.Cosmos.Spatial;
+using Newtonsoft.Json;
+
+namespace LocationData
+{
+    public interface IPlace
+    {
+        [JsonProperty("id")]
+        string Id { get; set; }
+
+        [JsonProperty("name")]
+        string Name { get; set; }
+
+        [JsonProperty("address")]
+        string Address { get; set; }
+
+        [JsonProperty("location")]
+        Point Location { get; set; }
+    }
+}
